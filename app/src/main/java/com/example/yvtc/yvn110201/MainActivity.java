615bbfc2e6fad6381e636461b86d9358d319f811
shaojung <shaojung@gmail.com>
2016-11-02 20:29:54 +0800

    add button programally

--- app/src/main/java/com/example/yvtc/yvn110201/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn110201/MainActivity.java
@@ -2,15 +2,24 @@ package com.example.yvtc.yvn110201;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
         Button btn = new Button(MainActivity.this);
-        setContentView(btn);
+        btn.setText("Click!!");
+        Button btn2 = new Button(MainActivity.this);
+        btn2.setText("Click 2222");
+        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_main);
+        layout.addView(btn);
+        layout.addView(btn2);
+
     }
 }

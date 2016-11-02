f27dddc26b9e8b7a61797153096757ceaaa2ddf3
shaojung <shaojung@gmail.com>
2016-11-02 21:31:27 +0800

    FrameLayout

--- app/src/main/java/com/example/yvtc/yvn110201/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn110201/MainActivity.java
@@ -5,21 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn444;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout5);
+        btn444 = (Button) findViewById(R.id.button15);
+        btn444.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn444.setVisibility(View.INVISIBLE);
+            }
+        });
 
-        Button btn = new Button(MainActivity.this);
-        btn.setText("Click!!");
-        Button btn2 = new Button(MainActivity.this);
-        btn2.setText("Click 2222");
-        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_main);
-        layout.addView(btn);
-        layout.addView(btn2);
+//        Button btn = new Button(MainActivity.this);
+//        btn.setText("Click!!");
+//        Button btn2 = new Button(MainActivity.this);
+//        btn2.setText("Click 2222");
+//        TextView tv = new TextView(MainActivity.this);
+//        tv.setText("Hello World");
+//        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_main);
+//        layout.addView(btn);
+//        layout.addView(btn2);
+//        layout.addView(tv);
 
     }
+
+
 }
